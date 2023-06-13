a31b4c0fd9d6d5a25766897f4f1df0467fa40974
jackpot-yan <wikileaks928@hotmail.com>
2023-06-14 06:18:26 +0800

    联系页面完成

--- src/views/information.vue
+++ src/views/information.vue
@@ -1,5 +1,119 @@
 <template>
   <div id="information">
-    此处为个人信息
+    <div id="info_top">
+      <div class="info_title">
+        联系我
+      </div>
+    </div>
+    <div id="info_table">
+      <div class="table_title">
+        期待您的来信
+        <br>
+        <span class="tips" style="">期待与您一起共事!</span>
+      </div>
+      <div class="table">
+        <el-form :inline="true" :model="formInline" class="demo-form-inline"
+                 style="padding-top: 130px;padding-left: 50px">
+          <el-form-item label="企业名称" required=true>
+            <el-input v-model="formInline.user"/>
+          </el-form-item>
+          <el-form-item label="联系方式" required=true>
+            <el-input v-model="formInline.contact"/>
+          </el-form-item>
+          <el-form-item label="内容" prop="desc">
+            <el-input v-model="formInline.region" type="textarea" style="width: 400px"/>
+          </el-form-item>
+          <br>
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">Query</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
+<script lang="ts" setup>
+import {reactive} from 'vue'
+
+const formInline = reactive({
+  user: '',
+  contact: '',
+  region: '',
+})
+
+const onSubmit = () => {
+  console.log(formInline)
+}
+</script>
+<style>
+#information {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.info_title {
+  text-align: center;
+  font-size: 44px;
+  font-weight: normal;
+  font-style: normal;
+  color: #ffffff;
+  margin-top: 60px;
+}
+
+.el-radio-group {
+  margin-right: 12px;
+}
+
+#info_table {
+  background-image: url("../assets/table.jpg");
+  background-size: cover;
+  margin-top: 30px;
+  margin-left: 200px;
+  margin-right: 200px;
+  height: 70%;
+  display: flex;
+  text-align: left;
+}
+
+.table_title {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  -webkit-text-size-adjust: none;
+  word-spacing: normal;
+  font-family: "微软雅黑", Helvetica, Arial, Verdana, sans-serif;
+  font-style: normal;
+  color: #000000;
+  visibility: visible;
+  line-height: 1.5em;
+  word-wrap: break-word;
+  letter-spacing: 1px;
+  font-weight: 700;
+  padding-left: 50px;
+  padding-top: 50px;
+  box-sizing: border-box;
+  font-size: 22px;
+}
+
+.tips {
+  -webkit-text-size-adjust: none;
+  word-spacing: normal;
+  font-family: "微软雅黑", Helvetica, Arial, Verdana, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  visibility: visible;
+  color: #828282;
+  line-height: 1.5em;
+  word-wrap: break-word;
+  letter-spacing: 1px;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 15px;
+}
+
+el-form {
+  padding: 50px;
+}
+</style>
--- src/views/skill.vue
+++ src/views/skill.vue
@@ -1,5 +1,87 @@
 <template>
   <div id="skill">
-    此处为技能
+    <div id="skill_left">
+      <div class="skill_title">
+        专业技能
+      </div>
+      <div class="schedule_title">
+        Python
+      </div>
+      <el-progress :text-inside="true" :stroke-width="26" :percentage="70"/>
+      <div class="schedule_title">
+        前端(Vue)
+      </div>
+      <el-progress
+          :text-inside="true"
+          :stroke-width="26"
+          :percentage="30"
+          status="success"
+      />
+      <div class="schedule_title">
+        GoLang
+      </div>
+      <el-progress
+          :text-inside="true"
+          :stroke-width="26"
+          :percentage="60"
+          status="warning"
+      />
+      <div class="schedule_title">
+        Docker(k8s)
+      </div>
+      <el-progress
+          :text-inside="true"
+          :stroke-width="26"
+          :percentage="40"
+          status="exception"
+      />
+    </div>
+    <div id="skill_right">
+      <div id="skill_image">
+        <img src="../../public/skill_image.webp"
+             style="width: 60%;height: 60%; padding-top: 150px;display: flex;justify-content: flex-start"/>
+      </div>
+    </div>
   </div>
 </template>
+<style>
+#skill {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+}
+
+#skill_left {
+  height: 100%;
+  width: 50%;
+}
+
+.skill_title {
+  text-align: left;
+  font-family: f5f487082c474e4472d76201c069f2098;
+  font-size: 44px;
+  font-weight: normal;
+  font-style: normal;
+  color: #ffffff;
+  margin-top: 140px;
+  margin-left: 50px;
+  margin-bottom: 10px;
+}
+.schedule_title {
+  text-align: left;
+  font-family: f5f487082c474e4472d76201c069f2098;
+  font-size: 15px;
+  font-weight: normal;
+  font-style: normal;
+  color: #ffffff;
+  margin-top: 10px;
+  margin-left: 50px;
+  margin-bottom: 30px;
+}
+
+#skill_right {
+  height: 100%;
+  width: 50%;
+}
+</style>
